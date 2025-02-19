1e6ea77546d96321aa992979dcb24d19ae4de3a0
omerdirem <omerdirem@gmail.com>
2025-02-20 00:53:16 +0300

    Update Home.tsx

--- Home.tsx
+++ Home.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import { supabase } from "./lib/supabaseClient";
+import { supabase } from "./lib/supabaseclient";
 
 export default function Home() {
   const router = useRouter();
