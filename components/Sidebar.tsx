01b77ca0da7e0e26c7581dd3788650d9836873f5
omerdirem <omerdirem@gmail.com>
2025-02-20 00:04:47 +0300

    Create Sidebar.tsx

---
+++ components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <h2>Sidebar</h2>
+      {/* Sidebar içeriğini buraya ekleyebilirsiniz */}
+    </div>
+  );
+};
+
+export default Sidebar;
