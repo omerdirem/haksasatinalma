8e04a93ec4368f2a047ddaee7f00fc3972e529b8
omerdirem <omerdirem@gmail.com>
2025-02-19 17:52:57 +0300

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to My Purchase App</h1>
+      <p>This is a minimal Next.js project created via GitHub web UI.</p>
+    </div>
+  )
+}
